0f0a8fee7158ad49a8a3a10a2f670c174aafc237
tacodes <64085493+tim-andes@users.noreply.github.com>
2021-06-14 15:54:43 -0600

    Add files via upload

---
+++ image/tpl/default.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @var $title
+ * @var $title_position
+ * @var $url
+ * @var $link_attributes
+ * @var $new_window
+ * @var $attributes
+ * @var $classes
+ */
+?>
+
+<?php if( $title_position == 'above' ) : ?>
+	<?php echo $args['before_title'] . $title . $args['after_title']; ?>
+<?php endif; ?>
+
+<?php
+
+?>
+<div class="sow-image-container">
+<?php if ( ! empty( $url ) ) : ?><a href="<?php echo sow_esc_url( $url ) ?>" <?php foreach( $link_attributes as $att => $val ) if ( ! empty( $val ) ) : echo $att.'="' . esc_attr( $val ) . '" '; endif; ?>><?php endif; ?>
+	<img <?php foreach( $attributes as $n => $v ) if ( $n === 'alt' || ! empty( $v ) ) : echo $n.'="' . esc_attr( $v ) . '" '; endif; ?>
+		class="<?php echo esc_attr( implode(' ', $classes ) ) ?>"/>
+<?php if ( ! empty( $url ) ) : ?></a><?php endif; ?>
+</div>
+
+<?php if ( ! empty( $caption ) ) : ?>
+	<div class="sow-image-caption-container">
+		<div style="width:<?php echo $attributes['width']; ?>px;" class=sow-image-caption><small><em><?php echo $caption; ?></em></small></div>
+	</div>
+<?php endif; ?>
+
+<?php if( $title_position == 'below' ) : ?>
+	<?php echo $args['before_title'] . $title . $args['after_title']; ?>
+<?php endif; ?>
